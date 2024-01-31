de6e1442eac015de2823eb86d7639fa71079c317
charlesliu04 <141584452+charlesliu04@users.noreply.github.com>
2024-01-31 18:05:43 -0500

    Add files via upload

---
+++ scriptblog.js
@@ -0,0 +1,28 @@
+const typed = new Typed('#typed-text', {
+    strings: ['{Che Liu - Blog}'],
+    typeSpeed: 30,
+    startDelay: 800, 
+    loop: false,
+    showCursor: true, 
+    cursorChar: '|', 
+    onComplete: (self) => {
+      
+      setTimeout(() => {
+        
+        self.cursor.style.display = 'none';
+  
+        const introDiv = document.getElementById('animated-intro');
+        introDiv.style.animation = 'moveUp 1s forwards';
+  
+        introDiv.addEventListener('animationend', () => {
+          const overlay = document.getElementById('fullscreen-overlay');
+          overlay.style.animation = 'fadeOut 1s forwards';
+  
+          overlay.addEventListener('animationend', () => {
+       
+          });
+        });
+      }, 700); 
+    }
+  });
+  
